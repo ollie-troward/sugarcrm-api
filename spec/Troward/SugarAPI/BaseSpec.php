3deb5f3384f06f098995b885cf4a08894fc88af1
Ollie Troward <ollie@ollietroward.com>
2015-05-15 11:09:57 +0100

    Exception updated

--- spec/Troward/SugarAPI/BaseSpec.php
+++ spec/Troward/SugarAPI/BaseSpec.php
@@ -51,11 +51,14 @@ class BaseSpec extends ObjectBehavior {
      */
     protected function it_locates_and_parses_the_configuration()
     {
-        $file = file_get_contents($this->location);
-
-        if (!$file) throw new ClientException("Missing config.yml file for tests");
-
-        return Yaml::parse($file);
+        try
+        {
+            $file = file_get_contents($this->location);
+            return Yaml::parse($file);
+        } catch (\Exception $e)
+        {
+            throw new ClientException("Missing config.yml file for tests");
+        }
     }
 
     /**
